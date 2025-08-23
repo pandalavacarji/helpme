d29deedcac7d06d30c6239daa47459c5e188ee15
pandalavacarji <120312335+pandalavacarji@users.noreply.github.com>
2025-08-23 19:38:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "FastAPI Turnstile Controller is running!"}
+
+@app.post("/open_turnstile/")
+async def open_turnstile(product_key: str, device_name: str, door: int = 0):
+    return {
+        "result": "received",
+        "product_key": product_key,
+        "device_name": device_name,
+        "door": door
+    }
